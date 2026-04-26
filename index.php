eec4a0885f78d5a90a94b53316d518b8618acc0b
Rica Mae Consigo <23-78826@g.batstate-u.edu.ph>
2026-04-26 11:10:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <title>CI/CD Demo</title>
 </head>
-<body style="background-color:blue">
+<body style="background-color:red">
     <h1 id="title">Hello CI/CD rtials</h1>
 
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCtbwmxsM1LCh9SK1zGhDpeAFKSjkmFcsGNg&s" width="150" height="150">
